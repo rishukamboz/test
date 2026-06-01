8111563b3e5eac5a0d6fc1f8adad28f828f59675
rishukamboz <kambojrishav7@gmail.com>
2026-06-01 14:16:27 +0530

    Update app.py

--- camera_app/app.py
+++ camera_app/app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Website Working!</h1>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return render_template("index.html")
